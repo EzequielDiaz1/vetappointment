fc45526d425b1fc95d51e047f19cafc96ff00b6b
EzequielDiaz1 <ezequiel.diaz1337@gmail.com>
2020-10-26 21:37:54 -0300

    Se realiza el formulario,  el componente para cargar citas y eliminar las mismas.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, useState} from 'react'
+import Formulario from './components/Formularios'
+import Cita from './components/Cita'
 
 function App() {
+
+  const [citas, setCitas] = useState([])
+
+
+  //Funcion que toma las citas actuales y agrega la nueva
+  const crearCita = cita => {
+    setCitas([
+      ...citas,
+      cita
+    ])
+  }
+
+  //Función que elimina una cita por su id
+  const eliminarCita = id => {
+    const nuevasCitas = citas.filter(cita => cita.id !== id)
+    setCitas(nuevasCitas)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h1>Administrador de pacientes </h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario crearCita={crearCita}/>
+          </div>    
+          <div className="one-half column">
+            <h2>Administra tus citas</h2>
+            {citas.map(cita => (
+              <Cita
+                key={cita.id}
+                cita={cita}
+                eliminarCita={eliminarCita}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+        
+    </Fragment>
   );
 }
 
---
+++ src/components/Formularios.jsx
@@ -0,0 +1,113 @@
+import React, {useState} from 'react';
+import shortid  from 'shortid';
+
+const Formulario = ({crearCita}) => {
+
+    const [cita, setCita] = useState({
+        mascota: '',
+        propietario: '',
+        fecha: '',
+        hora: '',
+        sintomas: ''
+    });
+
+    const [error, setError] = useState(false)
+
+
+    const handlerChange = e => {
+        setCita({
+            ...cita,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const { mascota, propietario, fecha, hora, sintomas } = cita
+
+    const submitCita = (e) => {
+        e.preventDefault();
+        // Validar
+        if(mascota.trim() === '' || propietario.trim() === '' || fecha.trim() === '' || hora.trim() === ''  || sintomas.trim() === '' ){
+            setError(true)
+            return;
+        }
+        // Eliminar el mensaje de error
+        setError(false)
+        // Asignar un ID
+        cita.id = shortid();
+        // Crear la cita
+        crearCita(cita)
+        // Reiniciar el form
+        setCita({
+            mascota: '',
+            propietario: '',
+            fecha: '',
+            hora: '',
+            sintomas: ''
+        })
+    }
+
+    return ( 
+        <div>
+            <h2>Crear Cita</h2>
+
+            { error ? <p className="alerta-error">Todos los campos son obligatorios</p>: null}
+
+            <form onSubmit={submitCita}>
+                <label>Nombre Mascota</label>
+                <input 
+                    type="text"
+                    name="mascota"
+                    className="u-full-width"
+                    placeholder="Nombre de mascota"
+                    onChange={handlerChange}
+                    value={mascota}
+                />
+
+                <label>Nombre Dueño</label>
+                <input 
+                    type="text"
+                    name="propietario"
+                    className="u-full-width"
+                    placeholder="Nombre de dueño"
+                    onChange={handlerChange}
+                    value={propietario}
+                />
+
+                <label>Fecha</label>
+                <input 
+                    type="date"
+                    name="fecha"
+                    className="u-full-width"
+                    onChange={handlerChange}
+                    value={fecha}
+                />
+
+                <label>Hora</label>
+                <input 
+                    type="time"
+                    name="hora"
+                    className="u-full-width"
+                    onChange={handlerChange}
+                    value={hora}
+                />
+
+                <label>Sintomas</label>
+                <textarea
+                    className="u-full-width"
+                    name="sintomas"
+                    onChange={handlerChange}
+                    value={sintomas}
+                ></textarea>
+
+                <button 
+                    type="submit"
+                    className="u-full-width button-primary"
+                >
+                    Agregar Cita
+                </button>
+            </form>
+        </div>
+    );
+}
+ 
+export default Formulario;
